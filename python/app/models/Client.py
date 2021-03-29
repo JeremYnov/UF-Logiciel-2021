559d3a17ed2de9eb06497636cfff90dd5026eb87
DavidMTdev <david.teixeira31@gmail.com>
2021-03-29 15:04:50 +0200

    Add findAll Client to ClientController

--- python/app/models/Client.py
+++ python/app/models/Client.py
@@ -11,10 +11,44 @@ class Client(db.Document):
     creation = db.DateTimeField(required=True, default=datetime.now())
 
     @staticmethod
+    def findAll():
+        try:
+            clients = Client.objects.all()
+            results = []
+
+            for client in clients:
+                data = {}
+                data['id'] = str(client.id)
+                data['firstName'] = client.firstName
+                data['lastName'] = client.lastName
+                data['email'] = client.email
+                data['creation'] = client.creation.strftime('%d/%m/%Y')
+
+                results.append(data)
+                
+        except Exception as error:
+            print(error)
+
+        return results
+
+    @staticmethod
+    def findById(body):
+        try:
+            client = Client.from_json(json.dumps(body), True)
+            client.save()
+        except Exception as error:
+            print(error)
+
+        return client
+
+
+    @staticmethod
     def createClient(body):
         try:
             client = Client.from_json(json.dumps(body), True)
             client.save()
+
+            print(client.to_mongo())
         except Exception as error:
             print(error)
 
