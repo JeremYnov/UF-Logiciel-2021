0d58617ad1841b44617c182d33241fc2d83748fa
JeremYnov <jeremy.richard@ynov.com>
2021-03-29 15:22:32 +0200

    added page clients

---
+++ vue/src/views/product/Product.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="products">
+
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      info: null,
+      errored: false,
+      loading: true,
+      product:{
+          name:null,
+          stock:null,
+          picture:null,
+          invoiceReference:null,
+      }
+    };
+  },
+  mounted: function () {
+    axios
+      .get("")
+      .then((response) => (this.info = response))
+      .catch((error) => {
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
+  },
+};
+</script>
+
+<style>
+
+</style>
