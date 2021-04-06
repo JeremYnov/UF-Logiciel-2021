ab4e5a4d98db83264e0ae43f856645a3f41caa69
loux22 <louis.ardilly@yahoo.fr>
2021-04-06 14:12:11 +0200

    add modify and delete product

--- python/app/controllers/__init__.py
+++ python/app/controllers/__init__.py
@@ -3,5 +3,5 @@ from app.controllers.ProductController import ProductController
 
 def routes(api):
      api.add_resource(ClientController, '/api/clients', '/api/client/<string:id>', '/api/client/<string:id>/update', '/api/client/new')
-     api.add_resource(ProductController, '/api/product/<string:id>/update', '/api/product/new', '/api/product/<string:id>', '/api/products')
+     api.add_resource(ProductController, '/api/product/<string:id>/update', '/api/product/new', '/api/product/<string:id>', '/api/products', '/api/product/<string:id>/delete')
    
