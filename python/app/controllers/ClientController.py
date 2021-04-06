7a0ceb3e657ce28c9e40c93af407e2ae7a32a05e
DavidMTdev <david.teixeira31@gmail.com>
2021-04-06 17:19:34 +0200

    update getAllclient

--- python/app/controllers/ClientController.py
+++ python/app/controllers/ClientController.py
@@ -13,6 +13,7 @@ class ClientController(Resource):
             result = {
                 'message': "liste de tout les clients",
                 'success': True,
+                'unitName': "Client",
                 'results': clients
             }
 
@@ -24,7 +25,12 @@ class ClientController(Resource):
             result = {
                 'message': f"data client {client['id']}",
                 'success': True,
-                'result': client
+                'result': client,
+                'form': {
+                    'firstName': "text",
+                    'lastName': "text",
+                    'email': "email",
+                }
             }
 
             return jsonify(result)
@@ -41,7 +47,7 @@ class ClientController(Resource):
 
         client = Client.create(body)
 
-        if client != 500:
+        if not client:
             result = {
                 'message': f"data client created",
                 'success': True,
@@ -59,22 +65,47 @@ class ClientController(Resource):
         
 
     def put(self, id=""):
-        body = request.json
-        
-        updated = Client.update(id, body)
+        if str(request.url_rule) == '/api/client/<string:id>/update':
+            body = request.json
+            
+            updated = Client.update(id, body)
 
-        if updated != 0:
+            if updated != 0:
+                result = {
+                    'message': f"data client {id} update",
+                    'success': True,
+                    'updated': updated
+                }
+
+                return jsonify(result)
+
             result = {
-                'message': f"data client {id} update",
-                'success': True,
-                'updatec': updated
+                'message': f"data client {id} not update",
+                'success': False,
             }
 
             return jsonify(result)
 
-        result = {
-            'message': f"data client {id} not update",
-            'success': False,
-        }
+        return Response(status=404)
 
-        return jsonify(result)
+    
+    def delete(self, id=""):
+        if str(request.url_rule) == '/api/client/<string:id>/delete':
+            client = Client.delete(id)
+
+            if not client:
+                result = {
+                "message": f"data client not delete",
+                "success": False
+                }
+
+                return jsonify(result)
+
+            result = {
+                "message": f"data client {id} delete",
+                "success": True
+            }
+
+            return jsonify(result)
+
+        return Response(status=404)
