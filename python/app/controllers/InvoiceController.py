6d3f0fa9fb5bd8e7253d349a00599f8623419bf7
loux22 <louis.ardilly@yahoo.fr>
2021-04-19 16:14:09 +0200

    add read and delete invoice

--- python/app/controllers/InvoiceController.py
+++ python/app/controllers/InvoiceController.py
@@ -6,9 +6,52 @@ import json
 from app.models.Invoice import Invoice
 
 class InvoiceController(Resource):
+    def get(self, id="") :
+        
+        if str(request.url_rule) == '/api/invoices':
+
+            invoices = Invoice.getAllInvoice()
+            result = {
+                "message": 'recuperation des factures',
+                "success": True,
+                "count" : len(invoices),
+                "results": invoices,
+                "unitName": "invoice"
+            }
+
+            return jsonify(result)
+        
+        elif str(request.url_rule) == '/api/invoice/<string:id>' :
+            invoice = Invoice.getInvoice(id)
+            result = {
+                "message": 'recuperation de la facture ' + str(invoice["id"]),
+                "success": True,
+                "count" : 1,
+                "result": invoice
+            }
+
+            return jsonify(result)
+
+        return Response(status=404)
     def post(self):
         body = request.json
 
         invoice = Invoice.create(body)
 
         return Response(invoice.to_json(), mimetype="application/json", status=200)
+    
+
+    def delete(self, id=""):
+        if str(request.url_rule) == '/api/invoice/<string:id>/delete':
+            
+            invoice = Invoice.deleteInvoice(id)
+            result = {
+                "message": "la suppresion a bien été faites",
+                "success": True
+            }
+            if not invoice:
+                result["message"] = "le produit n'existe pas, il ne peut pas y avoir de suppresion"
+                result["success"] = False
+            return jsonify(result)
+
+        return Response(status=404)
