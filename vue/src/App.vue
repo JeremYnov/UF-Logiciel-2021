0a0da9a3af3a4862028693abf598c386ac6ea199
Jérémy <jeremy.richard@ynov.com>
2021-04-26 20:41:49 +0200

    added new style on page DiplayAllVue (TODO : UnitVue, Add, Update)

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,39 +1,141 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <Navbar/>
     <router-view :key="$route.fullPath" class="router-view"/>
+  </div> -->
+  <div id="app">
+    <div class="navigation">
+      <ul>
+        <li>
+          <router-link class="dropdown-item" to="/">
+            <span class="icon"
+              ><i class="fas fa-home" aria-hidden="true"></i
+            ></span>
+            <span class="title">Home</span></router-link
+          >
+        </li>
+        <li>
+          <router-link class="dropdown-item" to="/clients">
+            <span class="icon"><i class="fas fa-users"></i></span>
+            <span class="title">Clients</span>
+          </router-link>
+        </li>
+        <li>
+          <router-link class="dropdown-item" to="/products">
+            <span class="icon"><i class="fas fa-apple-alt"></i></span>
+            <span class="title">Produits</span></router-link
+          >
+        </li>
+        <li>
+          <router-link class="dropdown-item" to="/invoices">
+            <span class="icon"><i class="fas fa-file-invoice-dollar"></i></span>
+            <span class="title">Factures</span>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="content">
+      <router-view :key="$route.fullPath" class="router-view" />
+    </div>
   </div>
 </template>
 <script>
-import Navbar from './components/layouts/Navbar'
+// import Navbar from './components/layouts/Navbar'
 export default {
-  components:{
-    Navbar,
-  }
-}
+  components: {
+    // Navbar,
+  },
+};
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;700&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Quicksand', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  .router-view{
-    padding: 20px;
-  }
 }
 
-#nav {
+// #nav {
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.content {
+  margin-left: 60px;
+  transition: 0.5s;
+}
+
+.navigation {
+  position: fixed;
+  width: 60px;
+  height: 100%;
+  background: #2c3e50;
+  transition: 0.5s;
+  overflow: hidden;
+  &:hover {
+    transition: 0.5s;
+    width: 250px;
+  }
+  &:hover ~ .content {
+    transition: 0.5s;
+    margin-left: 250px;
+  }
+  ul {
+    position: absolute;
+    top: 60px;
+    left: 0;
+    width: 100%;
+    li {
+      position: relative;
+      width: 100%;
+      list-style: none;
+      &:hover {
+        background: #395168;
+      }
+      a {
+        position: relative;
+        display: block;
+        width: 100%;
+        display: flex;
+        text-decoration: none;
+        color: #fff;
+        .icon {
+          position: relative;
+          display: block;
+          min-width: 60px;
+          height: 60px;
+          line-height: 60px;
+          text-align: center;
+        }
+        .title {
+          position: relative;
+          display: block;
+          padding: 0 10px;
+          height: 60px;
+          line-height: 60px;
+          text-align: start;
+          white-space: nowrap;
+        }
+      }
     }
   }
+
+  
 }
 </style>
--- vue/src/views/display-all/DisplayAll.vue
+++ vue/src/views/display-all/DisplayAll.vue
@@ -1,5 +1,8 @@
 <template>
-  <section class="clients">
+  <section>
+    <div class="section-title">
+      <h1>{{ this.$route.name }}</h1>
+    </div>
     <div v-if="errored" class="error">
       <p>
         Nous sommes désolés, nous ne sommes pas en mesure de récupérer ces
@@ -8,8 +11,12 @@
     </div>
     <div class="container">
       <div v-if="loading" class="loading">Chargement...</div>
-      <router-link class="btn btn-primary btn-lg btn-block" v-bind:to="{ name: 'Add'+this.$route.name.slice(0,-1)}">Add new</router-link>
-      <table class="table">
+      <router-link
+        class="btn btn-primary btn-lg btn-block"
+        v-bind:to="{ name: 'Add' + this.$route.name.slice(0, -1) }"
+        >Add new</router-link
+      >
+      <table class="content-table">
         <thead class="table-dark">
           <tr>
             <th v-for="key in keys" :key="key" scope="col">{{ key }}</th>
@@ -19,14 +26,24 @@
         <tbody>
           <tr v-for="info in infos" :key="info.id">
             <td v-for="(value, key) in info" :key="key">
-              <router-link v-bind:to="{ name: pathName, params: { id: info.id } }">
-                <img v-if="key == 'image'" :src="value.url" :alt="value.name" style="height:50px; width:50px">
+              <router-link
+                v-bind:to="{ name: pathName, params: { id: info.id } }"
+              >
+                <img
+                  v-if="key == 'image'"
+                  :src="value.url"
+                  :alt="value.name"
+                  style="height: 50px; width: 50px"
+                />
                 <p v-else>{{ value }}</p>
-                </router-link>
+              </router-link>
             </td>
             <td>
               <router-link
-                v-bind:to="{ name: 'Update'+pathName, params: { id: info.id } }"
+                v-bind:to="{
+                  name: 'Update' + pathName,
+                  params: { id: info.id },
+                }"
                 ><i class="fas fa-edit"></i
               ></router-link>
               |
@@ -46,7 +63,7 @@ export default {
     return {
       pathName: null,
       infos: null,
-      types:null,
+      types: null,
       keys: null,
       errored: false,
       loading: true,
@@ -69,17 +86,17 @@ export default {
         this.loading = false;
       });
   },
-  methods:{
+  methods: {
     async deleteElement(id) {
       await axios
-      .delete(`/api${this.$route.path.slice(0, -1)}/${id}/delete`)
-      .catch((error) => {
-        console.log(error);
-        this.errored = true;
-      })
-      .finally(() => {
-        this.loading = false;
-      });
+        .delete(`/api${this.$route.path.slice(0, -1)}/${id}/delete`)
+        .catch((error) => {
+          console.log(error);
+          this.errored = true;
+        })
+        .finally(() => {
+          this.loading = false;
+        });
       window.location.reload();
     },
   },
@@ -87,10 +104,65 @@ export default {
 </script>
 
 <style lang="scss">
-.fa-trash-alt {
-  color: rgb(170, 4, 4);
+.container {
+  margin: 40px;
 }
-thead tr th{
+
+thead tr th {
   text-transform: capitalize;
+}
+
+.section-title {
+  width: 100%;
+  height: 60px;
+  background-color: #2c3e50;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  h1 {
+    color: white;
+  }
+}
+
+.content-table {
+  width: 100%;
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 0.9em;
+  min-width: 400px;
+  border-radius: 5px 5px 0 0;
+  overflow: hidden;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+  thead tr {
+    background-color: #2c3e50;
+    color: #fff;
+    text-align: center;
+    font-weight: bold;
+  }
+  th,
+  td {
+    padding: 12px 15px;
+    font-size: 15px;
+    .fa-trash-alt {
+        color: rgb(170, 4, 4);
+      }
+  }
+  tbody tr {
+    border-bottom: 1px solid #dddddd;
+    &:nth-of-type(even) {
+      background-color: #f3f3f3;
+    }
+    &:last-of-type {
+      border-bottom: 2px solid #2c3e50;
+    }
+    td a {
+      text-decoration: none;
+      color: black;
+      .fa-edit{
+        color: rgb(22, 131, 255);
+      }
+      
+    }
+  }
 }
 </style>
