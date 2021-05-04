fee74c4dac55085004e062a4f7f4446d6b45461d
DavidMTdev <david.teixeira31@gmail.com>
2021-05-04 15:18:56 +0200

    add docker

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	devServer : {
 		proxy : {
 			'/api' : {
-				target       : 'http://localhost:5000',
+				target       : 'http://python-api:5000',
 				changeOrigin : true,
 				logLevel     : 'debug'
 			}
